eed684cab1c921587e26f738c110c42f5e26e3f8
bezchekera <aniaruyceva@mail.ru>
2018-09-19 20:15:23 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  
+  let count= Math.floor(number/5)+Math.floor(number/25)+Math.floor(number/125)
+ +Math.floor(number/625)+Math.floor(number/3125)
+  +Math.floor(number/15625)+Math.floor(number/78125)
+ +Math.floor(number/390625)+Math.floor(number/1953125) 
+  +Math.floor(number/9765625)+Math.floor(number/48828125) ;// your implementation
+
+
+  return count;
 }
